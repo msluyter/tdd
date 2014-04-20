a0cf9b013009ebdc5829913cda89a3d5382514fe
Mike Sluyter <msluyter@gmail.com>
2014-04-20 18:22:31 -0500

    commiting tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,18 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpResponse, HttpRequest
 from django.test import TestCase
+from .views import home_page
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  #2
+        self.assertEqual(found.func, home_page)  #3
 
 
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #5
